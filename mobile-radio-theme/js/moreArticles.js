064f1e21f85bc9671542dfa5100a5798dbb7fe43
scaphandroid <arl-informatique@gmx.ch>
2017-01-19 11:58:50 +0100

    ajout licence open source

--- mobile-radio-theme/js/moreArticles.js
+++ mobile-radio-theme/js/moreArticles.js
@@ -1,3 +1,22 @@
+/**
+ *(c) Alexis Raphaeloff 2017 (alexis@raphaeloff.net)
+ *
+ *This file is part of mobile-radio-theme.
+ *
+ *mobile-radio-theme is free software: you can redistribute it and/or modify
+ *it under the terms of the GNU General Public License as published by
+ *the Free Software Foundation, either version 3 of the License, or
+ *(at your option) any later version.
+ *
+ *mobile-radio-theme is distributed in the hope that it will be useful,
+ *but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *GNU General Public License for more details.
+ *
+ *You should have received a copy of the GNU General Public License
+ *along with mobile-radio-theme.  If not, see http://www.gnu.org/licenses/
+ */
+
 //pour charger 10 articles de plus lors du clic sur "charger plus d'article"
 
 /***** EN CAS DE SOUCIS OU D'USAGE PARTICULIER VOUS POUVEZ PERSONNALISER CES DEUX VALEURS  *****/
--- mobile-radio-theme/single.php
+++ mobile-radio-theme/single.php
@@ -1,4 +1,23 @@
 <?php
+/**
+ *(c) Alexis Raphaeloff 2017 (alexis@raphaeloff.net)
+ *
+ *This file is part of mobile-radio-theme.
+ *
+ *mobile-radio-theme is free software: you can redistribute it and/or modify
+ *it under the terms of the GNU General Public License as published by
+ *the Free Software Foundation, either version 3 of the License, or
+ *(at your option) any later version.
+ *
+ *mobile-radio-theme is distributed in the hope that it will be useful,
+ *but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *GNU General Public License for more details.
+ *
+ *You should have received a copy of the GNU General Public License
+ *along with mobile-radio-theme.  If not, see http://www.gnu.org/licenses/
+ */
+
     $ajaxRequest = false;
     if( isset( $_SERVER['HTTP_X_REQUESTED_WITH'] ) && $_SERVER['HTTP_X_REQUESTED_WITH']== 'VOSTOKAJAXREQUEST' ){
         $ajaxRequest = true;
